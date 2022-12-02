dc149a0d98b46f44f4eff269d4754c124dda0f24
ShishirRijal <ccrrizal@gmail.com>
2022-12-02 14:24:55 +0545

    screens added

---
+++ lib/screens/rent_screen/rent_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RentScreen extends StatefulWidget {
+  const RentScreen({super.key});
+
+  @override
+  State<RentScreen> createState() => RentScreenState();
+}
+
+class RentScreenState extends State<RentScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
